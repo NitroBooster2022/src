2f89244b6d60649f26c8965b3ddd45b7b9b1520f
AntoineDeng <alexander.deng@mail.mcgill.ca>
2023-02-17 21:38:18 -0500

    hbf

--- control/scripts/encoder.py
+++ control/scripts/encoder.py
@@ -13,9 +13,8 @@ from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 # from pynput import keyboard
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32
 from std_msgs.msg import Header
-from utils.msg import Encoder
 from message_filters import ApproximateTimeSynchronizer
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Twist, Vector3
@@ -25,33 +24,34 @@ class EncoderNode():
     def __init__(self):
         print("init encoder node")
         rospy.init_node('encoder_node', anonymous=True)
-        self.image_sub = rospy.Subscriber("/gazebo/model_states", ModelStates, queue_size=3)
-        # self.image_sub = rospy.Subscriber("automobile/image_raw/compressed", CompressedImage, self.image_callback)
-        self.pub = rospy.Publisher("encoder", Encoder, queue_size = 3)
-        self.p = Encoder()
+        self.image_sub = rospy.Subscriber("/automobile/encoder", Float32, self.callback, queue_size=3)
+        # self.image_sub = rospy.Subscriber("/gazebo/model_states", ModelStates, queue_size=3)
+        # self.pub = rospy.Publisher("encoder", Encoder, queue_size = 3)
+        # self.p = Encoder()
         self.rate = rospy.Rate(15)
 
-    def image_callback(self, ModelStates):
+    def callback(self, data):
         """
         Callback function for the image processed topic
         :param data: Image data in the ROS Image format
         """
-        header = Header()
-        print("hi")
-        header.seq = ModelStates.header.seq
-        header.stamp = ModelStates.header.stamp
-        header.frame_id = ModelStates.header.frame_id
-        # Update the header information in the message
-        self.p.header = header
-        x_speed = ModelStates.twist[72].linear.x
-        y_speed = ModelStates.twist[72].linear.y
-        speed = math.sqrt(x_speed*x_speed+y_speed*y_speed)
-        self.p.speed = speed
-        t1 = time.time()
+        # header = Header()
+        # print("hi")
+        # header.seq = ModelStates.header.seq
+        # header.stamp = ModelStates.header.stamp
+        # header.frame_id = ModelStates.header.frame_id
+        # # Update the header information in the message
+        # self.p.header = header
+        # x_speed = ModelStates.twist[72].linear.x
+        # y_speed = ModelStates.twist[72].linear.y
+        # speed = math.sqrt(x_speed*x_speed+y_speed*y_speed)
+        # self.p.speed = speed
+        # t1 = time.time()
 
-        print(self.p)
-        self.pub.publish(self.p)
-        print("time: ", time.time()-t1)
+        # print(self.p)
+        # self.pub.publish(self.p)
+        # print("time: ", time.time()-t1)
+        print(data.data)
 
     
 
